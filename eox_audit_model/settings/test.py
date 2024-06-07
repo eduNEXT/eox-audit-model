571aee5b0554be07b566fe3910c4bcdcec448a84
Luis Felipe Castano <felipe.castano@edunext.co>
2024-06-07 10:53:32 -0500

    feat: add github action integration

--- eox_audit_model/settings/test.py
+++ eox_audit_model/settings/test.py
@@ -4,6 +4,11 @@ Test Django settings for eox_audit_model project.
 
 from __future__ import unicode_literals
 
+import codecs
+import os
+
+import yaml
+
 from .common import *  # pylint: disable=wildcard-import
 
 ALLOWED_HOSTS = ['*']
@@ -49,3 +54,13 @@ LOGGING = {
 
 ALLOW_EOX_AUDIT_MODEL = True
 CELERY_TASK_ALWAYS_EAGER = False
+
+def plugin_settings(settings):  # pylint: disable=function-redefined
+    """
+    For the platform tests
+    """
+    # setup the databases used in the tutor local environment
+    if os.environ['LMS_CFG']:
+        with codecs.open(os.environ['LMS_CFG'], encoding='utf-8') as f:
+            env_tokens = yaml.safe_load(f)
+        settings.DATABASES = env_tokens['DATABASES']
